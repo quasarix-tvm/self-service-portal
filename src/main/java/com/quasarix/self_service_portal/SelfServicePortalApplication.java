72707c74d83e3fcaf3b6b97698494deeabefbf22
anto.jayaraj <anto.jayaraj@STS-2691.sts.co.in>
2024-06-06 17:15:22 +0530

    initial_commit

---
+++ src/main/java/com/quasarix/self_service_portal/SelfServicePortalApplication.java
@@ -0,0 +1,13 @@
+package com.quasarix.self_service_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfServicePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfServicePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quasarix/self_service_portal/SelfServicePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quasarix.self_service_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfServicePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
